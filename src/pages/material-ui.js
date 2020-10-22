332003a3a19f4ea8bf68839b42b228d0fc6ed932
yashpk789987 <yashpk789987@gmail.com>
2020-10-22 14:37:51 +0530

    set up done

---
+++ src/pages/material-ui.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+export default function MaterialUI() {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Standard" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+  );
+}
